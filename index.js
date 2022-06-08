9a9d9df8c50352fa87755327312cf376abb9594b
mori118 <gohiro1020@gmail.com>
2022-06-08 11:05:38 +0900

    hennkoui

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const piece01 = document.querySelector('.pos1');
 for (let n = 1; n <= 16; n = n + 1) {
-    const piece = document.querySelector('.pos-' + n);  
-
-
-  // MEMO: 1 ～ 16 でランダムに数値を決めると、番号が被る場合がある
-piece01.style.order = parseInt(Math.random() * 16) + 1;
-}
+    const piece = document.querySelector('.pos-' + n);
+  
+    // MEMO: 1 ～ 16 でランダムに数値を決めると、番号が被る場合がある
+    piece.style.order = parseInt(Math.random() * 16) + 1;
+  }
+  
+  function pieceClickHandler() {
+    console.log('ピースがクリックされました');
+  }
+  
+  const piece01 = document.querySelector('.pos-1');
+  
+  piece01.addEventListener('click', pieceClickHandler);
